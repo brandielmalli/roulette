317ec6588519aeb8fa39dec3f78f000dca08aa3a
brandielmalli <brandiduarte@icloud.com>
2018-11-30 14:40:05 -0500

    remove node_module folder

--- public/main.js
+++ public/main.js
@@ -1,45 +1,81 @@
-var thumbUp = document.getElementsByClassName("fa-thumbs-up");
-var trash = document.getElementsByClassName("fa-trash");
+var house=0,
+    player=0;
+document.getElementById("black").onclick=playerBetsBlack;
+function playerBetsBlack(){
+    var color = getRandomColor();
+    checkWhoWon(color,"black");
+}
 
-Array.from(thumbUp).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
-        fetch('messages', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg,
-            'thumbUp':thumbUp
-          })
-        })
-        .then(response => {
-          if (response.ok) return response.json()
-        })
-        .then(data => {
-          console.log(data)
-          window.location.reload(true)
-        })
-      });
-});
+document.getElementById("red").onclick=playerBetsRed;
+function playerBetsRed(){
+    var color = getRandomColor();
+    checkWhoWon(color,"red");
+}
+document.getElementById("green").onclick=playerBetsGreen;
+function playerBetsGreen(){
+    var color = getRandomColor();
+    checkWhoWon(color,"green");
+}
 
-Array.from(trash).forEach(function(element) {
-      element.addEventListener('click', function(){
-        const name = this.parentNode.parentNode.childNodes[1].innerText
-        const msg = this.parentNode.parentNode.childNodes[3].innerText
-        fetch('messages', {
-          method: 'delete',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            'name': name,
-            'msg': msg
-          })
-        }).then(function (response) {
-          window.location.reload()
-        })
-      });
-});
+function getRandomColor(){
+    var randomNumber=Math.random();
+    var choice="black";
+    if(randomNumber<.5){
+        choice="black";
+    }
+    else if(randomNumber<.9){
+        choice="red";
+    }
+    else {
+        choice="green";
+    }
+    return choice;
+}
+
+function checkWhoWon(choice,playerBets){
+       if(choice==playerBets){
+         increasePlayerMoney();
+    }
+    else{
+        decreasePlayerMoney();
+    }
+}
+function decreasePlayerMoney(){
+  let bet = parseFloat(document.getElementById('bet').value)
+    displayCompleteMessage("You Lose !");
+  fetch('messages', {
+    method: 'put',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      'name': name,
+      'bet': bet
+    })
+  })
+  .then(response => {
+    if (response.ok) return response.json()
+  })
+  .then(data => {
+    console.log(data)
+    window.location.reload(true)
+  })
+}
+function increasePlayerMoney(){
+  let bet = parseFloat(document.getElementById('bet').value)
+    displayCompleteMessage("Winner Winner !");
+  fetch('messages', {
+    method: 'put',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      'name': name,
+      'msg': msg,
+      'thumbUp':thumbUp
+    })
+  })
+  .then(response => {
+    if (response.ok) return response.json()
+  })
+  .then(data => {
+    console.log(data)
+    window.location.reload(true)
+  })
+}
